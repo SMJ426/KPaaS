c83444d11488df98709e6ebee304cde93dcdc9da
Fronted-minjae <shin.minjae@cashwalk.io>
2024-06-20 23:19:15 +0900

    test: UI 개선 작업을 위해서 product api를 모두 주석처리 후 작업합니다.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,52 @@
-'use server'
+'use server';
 
 import MainContainers from '@compoents/containers/MainContainers';
-import { cookies } from 'next/headers';
-import { LogingetPostsFile, getPostsFile } from '@compoents/util/post-util';
-import { fetchUserProfile } from '@compoents/util/http';
+// import { cookies } from 'next/headers';
+// import { LogingetPostsFile, getPostsFile } from '@compoents/util/post-util';
+// import { fetchUserProfile } from '@compoents/util/http';
 
 export default async function Home() {
-  const cookieStore = cookies();
-  const Authorization = cookieStore.get('Authorization');
-  
-  let authorizationValue = '';
-  if (Authorization) {
-    authorizationValue = Authorization.value || '';
-  }
+  // TODO: 주석 제거하고 나중에 다시
+  //   const cookieStore = cookies();
+  //   const Authorization = cookieStore.get('Authorization');
 
-  if (authorizationValue === '' || !Authorization) {
-    const postdata = await getPostsFile();
-    return (
-      <MainContainers postdata={postdata} accessToken={authorizationValue} />
-    );
-  } else {
-    const accessToken = decodeURIComponent(authorizationValue);
-    const profileData = await fetchUserProfile(accessToken);
-    const postdata = await LogingetPostsFile(accessToken, profileData.nick_name);
-    return (
-      <MainContainers postdata={postdata} accessToken={accessToken} nick_name={profileData.nick_name} />
-    );
-  }
+  //   let authorizationValue = '';
+  //   if (Authorization) {
+  //     authorizationValue = Authorization.value || '';
+  //   }
+
+  //   if (authorizationValue === '' || !Authorization) {
+  //     const postdata = await getPostsFile();
+  //     return (
+  //       <MainContainers postdata={postdata} accessToken={authorizationValue} />
+  //     );
+  //   } else {
+  //     const accessToken = decodeURIComponent(authorizationValue);
+  //     const profileData = await fetchUserProfile(accessToken);
+  //     const postdata = await LogingetPostsFile(
+  //       accessToken,
+  //       profileData.nick_name
+  //     );
+  //     return (
+  //       <MainContainers
+  //         postdata={postdata}
+  //         accessToken={accessToken}
+  //         nick_name={profileData.nick_name}
+  //       />
+  //     );
+  //   }
+  // }
+
+  // 임시로 빈 데이터를 사용해서 UI를 확인할 수 있도록 합니다.
+  const postdata = [];
+  const accessToken = '';
+  const nick_name = '';
+
+  return (
+    <MainContainers
+      postdata={postdata}
+      accessToken={accessToken}
+      nick_name={nick_name}
+    />
+  );
 }
-
--- src/containers/MainContainers.jsx
+++ src/containers/MainContainers.jsx
@@ -1,13 +1,11 @@
 'use client';
-import MainNavigation from "@compoents/components/layout/main-navigation";
-import { FaPen } from "react-icons/fa6";
-import Link from "next/link";
+import MainNavigation from '@compoents/components/layout/main-navigation';
+import { FaPen } from 'react-icons/fa6';
+import Link from 'next/link';
 import MiniCategoryComponents from '@compoents/components/minicategory/Minicategory';
 import CategoryComponents from '@compoents/components/minicategory/Category';
 import Pagination from '@compoents/components/pagination/Paginations';
-
-import styles from "./MainContainers.module.css";
-
+import styles from './MainContainers.module.css';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import CommuPosts from '@compoents/components/posts/commu-post';
@@ -17,25 +15,29 @@ export default function MainContainers({ postdata, accessToken, nick_name }) {
   const [selectedCategory, setSelectedCategory] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const PAGE_GROUP_SIZE = 5;
-  
-  
+
   const handlePageChange = (page) => {
     router.push(`/${page}`);
-  }
+  };
   const goToPreviousPageGroup = () => {
     setCurrentPage((prev) => prev - PAGE_GROUP_SIZE);
-  }
+  };
   const goToNextPageGroup = () => {
     setCurrentPage((prev) => prev + PAGE_GROUP_SIZE);
-  }
+  };
 
   const handleCategoryChange = (e) => {
     const categoryId = parseInt(e.target.id);
     if (Array.isArray(selectedCategory)) {
       if (selectedCategory.includes(categoryId)) {
-        setSelectedCategory(prevCategories => prevCategories.filter(id => id !== categoryId));
+        setSelectedCategory((prevCategories) =>
+          prevCategories.filter((id) => id !== categoryId)
+        );
       } else {
-        setSelectedCategory(prevCategories => [...prevCategories, categoryId]);
+        setSelectedCategory((prevCategories) => [
+          ...prevCategories,
+          categoryId,
+        ]);
       }
     } else {
       setSelectedCategory([categoryId]);
@@ -44,24 +46,33 @@ export default function MainContainers({ postdata, accessToken, nick_name }) {
 
   return (
     <>
-    <MainNavigation accessToken={accessToken} />
+      <MainNavigation accessToken={accessToken} />
       <div className={styles.pageContainer}>
         <section className={styles.flexSection1}></section>
         <section className={styles.flexSection2}>
           <div className={styles.buttonContainer}>
             <Link href="/newpost">
               <button className={styles.button}>
-                <FaPen className={styles.pencli}/>
+                <FaPen className={styles.pencli} />
                 <div className={styles.Add}>상품등록</div>
-                </button>
+              </button>
             </Link>
           </div>
           <div className={styles.cateSticky}>
             <CategoryComponents handleCategoryChange={handleCategoryChange} />
-            <MiniCategoryComponents className={styles.cateminibtn} selectedCategory={selectedCategory} onCategoryChange={handleCategoryChange}/>
+            <MiniCategoryComponents
+              className={styles.cateminibtn}
+              selectedCategory={selectedCategory}
+              onCategoryChange={handleCategoryChange}
+            />
           </div>
-          <CommuPosts posts={postdata} selectedCategory={selectedCategory} accessToken={accessToken} nick_name={nick_name} />
-          <Pagination 
+          <CommuPosts
+            posts={postdata}
+            selectedCategory={selectedCategory}
+            accessToken={accessToken}
+            nick_name={nick_name}
+          />
+          <Pagination
             currentPage={currentPage}
             posts={postdata}
             PAGE_GROUP_SIZE={PAGE_GROUP_SIZE}
