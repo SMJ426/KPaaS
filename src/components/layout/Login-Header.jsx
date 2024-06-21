4ad28dc8fbc3ed84ca1bdb44c66b3466a839df2f
Fronted-minjae <shin.minjae@cashwalk.io>
2024-06-22 01:07:28 +0900

    refactor : 1차 main 페이지 UI 개선 및 개선 사항 파악

--- src/components/layout/Login-Header.jsx
+++ src/components/layout/Login-Header.jsx
@@ -1,16 +1,15 @@
 'use client';
-import Link from "next/link";
+import Link from 'next/link';
 import styles from './Login-Header.module.css';
 
 export default function LoginHeader() {
-
-    return (
-        <>
-            <header className={styles.headerContainer}>
-                <Link href="/" legacyBehavior passHref>
-                    <div className={styles.logo}>다락방</div>
-                </Link>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header className={styles.headerContainer}>
+        <Link href="/" legacyBehavior passHref>
+          <div className={styles.logo}>다락방</div>
+        </Link>
+      </header>
+    </>
+  );
 }
--- src/components/layout/main-navigation.jsx
+++ src/components/layout/main-navigation.jsx
@@ -1,36 +1,128 @@
-import Link from "next/link";
-import FindEventSection from "../items/ItemSearch";
-
-
-import styles from './main-navigation.module.css'; 
-import SmallProfile from "../profile/SmallProfile";
+import Link from 'next/link';
+import FindEventSection from '../items/ItemSearch';
+import SmallProfile from '../profile/SmallProfile';
+import styled from 'styled-components';
 
 export default function MainNavigation({ accessToken }) {
-
-    
-
-    return (
-        <>
-            <header className={styles.headerContainer}>
-                <Link href="/" legacyBehavior passHref>
-                    <div className={styles.logo}>다락방</div>
-                </Link>
-                <div className={styles.navItem}>
-                    <FindEventSection accessToken={accessToken} />
-                </div>
-                {!accessToken && (
-                    <div className={styles.navItem3}>
-                        <Link href="/user/login" passHref>
-                            <button className={styles.navLink}>로그인</button>
-                        </Link>
-                    </div>
-                )}
-                {accessToken && (
-                    <div className={styles.navItem3}>
-                        <SmallProfile accessToken={accessToken}/>
-                    </div>
-                )}
-            </header>
-        </>
-    );
+  return (
+    <StyledWrapper>
+      <Link href="/" legacyBehavior passHref>
+        <a className="logo">장애인파이팅!</a>
+      </Link>
+      <div className="search-bar">
+        <FindEventSection accessToken={accessToken} />
+      </div>
+      {!accessToken && (
+        <div className="navItem3">
+          <Link href="/user/login" passHref>
+            <button className="navLink">로그인</button>
+          </Link>
+        </div>
+      )}
+      {accessToken && (
+        <div className="navItem3">
+          <SmallProfile accessToken={accessToken} />
+        </div>
+      )}
+    </StyledWrapper>
+  );
 }
+
+const StyledWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  background-color: #ffffff;
+  height: 130px;
+  border: none;
+  position: sticky;
+  top: 0;
+  z-index: 2;
+
+  .logo {
+    color: var(--black, #191a1c);
+    font-family: 'Gmarket Sans TTF';
+    font-size: 38px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    width: auto;
+    height: 64px;
+    margin-left: 10%;
+    border: none;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+  .search-bar {
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    width: 50%;
+    margin-left: 0px;
+  }
+
+  .navItem3 {
+    font-size: 0.5rem;
+    margin-top: 0px;
+    margin-left: 90%;
+    position: absolute;
+  }
+
+  .navLink {
+    width: 111px;
+    height: 48px;
+    border-radius: 10px;
+    border: 0;
+    background: rgba(73, 106, 243, 0.8);
+    margin-right: 50px;
+    color: #fff;
+    font-family: 'Pretendard Variable';
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 786px) {
+    .headerContainer {
+      height: 70px;
+    }
+
+    .logo {
+      margin-left: 30px;
+      font-size: 1rem;
+    }
+
+    .search-bar {
+      align-items: center;
+      justify-content: center;
+      margin-left: 0;
+      width: 100%;
+    }
+
+    .navItem3 {
+      font-size: 0.5rem;
+      margin-top: 0px;
+      margin-left: 87%;
+      position: absolute;
+    }
+
+    .navLink {
+      width: 41px;
+      height: 28px;
+      border-radius: 10px;
+      border: 0;
+      background: rgba(73, 106, 243, 0.8);
+      margin-right: 13%;
+      margin-top: 0px;
+      color: #fff;
+      font-family: 'Pretendard Variable';
+      font-size: 8px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      cursor: pointer;
+    }
+  }
+`;
