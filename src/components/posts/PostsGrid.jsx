7175a6b1f6d4def5db806507f0bc488f8118da49
Fronted-minjae <shin.minjae@cashwalk.io>
2024-07-10 23:13:22 +0900

    feat(media): 미디어 쿼리를 수정합니다.

--- src/components/posts/PostsGrid.jsx
+++ src/components/posts/PostsGrid.jsx
@@ -24,18 +24,21 @@ export default function PostsGrid({ postData, accessToken }) {
 
 const StyledWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   grid-gap: 24px;
+
+  @media (max-width: 2000px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media (max-width: 1625px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 
   @media (max-width: 1200px) {
     grid-template-columns: repeat(2, 1fr);
   }
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  @media (max-width: 480px) {
+  @media (max-width: 900px) {
     grid-template-columns: repeat(1, 1fr);
   }
 `;
