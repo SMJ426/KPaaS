b5f67f48bfc94c700eda9c5592a114d3ff371e02
Fronted-minjae <shin.minjae@cashwalk.io>
2024-07-31 00:09:27 +0900

    feat(footer): 푸터를 추가하고, global style 을 적용합니다.

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,11 +7,15 @@ const GlobalStyle = createGlobalStyle`
     margin-right:10%;
     padding: 0;
     box-sizing: border-box;
+    font-family:'Pretendard';
   }
 
   * {
     box-sizing: inherit;
   }
+
+
+
 `;
 
 export default GlobalStyle;
