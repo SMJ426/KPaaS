3357f43b55b3f0ca5ddcb58104937e399dd1893e
SMJ426 <jerry04260@gmail.com>
2024-08-11 19:40:08 +0900

    feat(chatting): 메세지 수신자와 발신자를 구별합니다.

--- src/components/chat/chatConversationPanel/ChatConversationPanel.jsx
+++ src/components/chat/chatConversationPanel/ChatConversationPanel.jsx
@@ -8,46 +8,30 @@ import RequestMessages from './RequestMessages';
 import ReceivedMessages from './ReceivedMessages';
 
 export default function ChatConversationPanel({ userInfo }) {
-  // WebSocket 클라이언트 인스턴스를 저장하는 상태
   const [stompClient, setStompClient] = useState(null);
   const [message, setMessage] = useState('');
-  // 보낸 메세지들을 모아둔 상태
-  const [requestMessages, setRequestMessages] = useState([]);
-  // 받은 메세지들을 모아둔 상태
-  const [receiveMessages, setReceiveMessages] = useState([]);
+  const [allMessages, setAllMessages] = useState([]);
 
-  // 이 아래 부턴 저의 이해를 돕기 위해 개인적인 주석도 포함되어있습니다.
-  // 물론, 기능 구현 되면 제거할 예정입니당
   useEffect(() => {
-    // WebSocket 연결 설정하는 부분
     const socket = new SockJS(
       'http://default-chat-service-7c2a3-25892552-1d82f05544d5.kr.lb.naverncp.com:50/ws'
     );
     const client = new Client({
       webSocketFactory: () => socket,
       onConnect: () => {
-        console.log('WebSocket 연결 성공 입니다!!!!!');
-
-        // 채팅방1에 메시지가 도착하면 메시지를 받아서 화면에 출력
         client.subscribe('/sub/room1', (msg) => {
-          console.log('123123 msg >>', msg);
-          console.log('456456 msg.body >>', msg.body);
           const receivedMessage = JSON.parse(msg.body);
-          console.log('구독한 메시지 수신:', receivedMessage);
 
-          // 받은 메시지가 자신이 보낸 메시지가 아닐 경우에만 추가
+          // 상대방에게 받은 메세지일때만 추가
           if (receivedMessage.sender.user_name !== userInfo.nick_name) {
-            setReceiveMessages((prevMessages) => [
+            setAllMessages((prevMessages) => [
               ...prevMessages,
-              receivedMessage,
+              { ...receivedMessage, type: 'received' },
             ]);
           }
         });
-
-        console.log('/sub/room1에 구독 성공'); // 구독 성공 로그
       },
 
-      // error 확인을 위해 header 와 body를 각각 로깅함
       onStompError: (frame) => {
         console.error('Broker reported error: ' + frame.headers['message']);
         console.error('Additional details: ' + frame.body);
@@ -64,14 +48,6 @@ export default function ChatConversationPanel({ userInfo }) {
     };
   }, []);
 
-  useEffect(() => {
-    console.log('보낸 메세지들 ', requestMessages);
-  }, [requestMessages]);
-
-  useEffect(() => {
-    console.log('받은 메세지들 ', receiveMessages);
-  }, [receiveMessages]);
-
   const sendMessage = () => {
     if (stompClient && stompClient.connected) {
       const messageBody = {
@@ -80,12 +56,16 @@ export default function ChatConversationPanel({ userInfo }) {
         type: 'TALK', // TALK or ENTER
         roomId: '1',
       };
-      console.log('전송중 >>', messageBody);
+
       stompClient.publish({
         destination: '/pub/chat/message',
         body: JSON.stringify(messageBody),
       });
-      setRequestMessages((prevMessages) => [...prevMessages, messageBody]); // 메시지 목록 업데이트
+      setAllMessages((prevMessages) => [
+        ...prevMessages,
+        { ...messageBody, type: 'sent' },
+      ]);
+
       setMessage('');
     } else {
       console.error('STOMP가 연결 안됐음');
@@ -95,18 +75,17 @@ export default function ChatConversationPanel({ userInfo }) {
   return (
     <StyledWrapper>
       <div className="wrapper-messages">
-        {/* 보낸 메세지 */}
-        {requestMessages.map((msg, index) => (
-          <RequestMessages
-            requestMessages={msg}
-            userInfo={userInfo}
-            key={index}
-          />
-        ))}
-        {/* 받은 메세지 */}
-        {receiveMessages.map((msg, index) => (
-          <ReceivedMessages key={index} receiveMessages={msg} />
-        ))}
+        {allMessages.map((msg, index) =>
+          msg.type === 'sent' ? (
+            <RequestMessages
+              requestMessages={msg}
+              userInfo={userInfo}
+              key={index}
+            />
+          ) : (
+            <ReceivedMessages key={index} receiveMessages={msg} />
+          )
+        )}
       </div>
       <div className="wrapper-input">
         <input
