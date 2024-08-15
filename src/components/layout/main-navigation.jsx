362a90a553d5fa53068e229138bd620bb8332ffd
SMJ426 <jerry04260@gmail.com>
2024-08-15 16:03:07 +0900

    refactor(modal): z-index 수정합니다.

--- src/components/layout/main-navigation.jsx
+++ src/components/layout/main-navigation.jsx
@@ -58,7 +58,8 @@ const StyledWrapper = styled.header`
   justify-content: space-between;
   position: sticky;
   top: 0;
-  z-index: 9;
+  /* TODO : 추후 모달 z-index 조절 필요 */
+  z-index: 10;
 
   background-color: #ffffff;
   height: 80px;
