3d78560bd9cc9a9f7ac6cf500b14efd01c71cfd9
SMJ426 <jerry04260@gmail.com>
2024-10-10 21:06:05 +0900

    chore(chatting-version-two): 콘솔 제거합니다.

--- src/components/chat/chatListPanel/ChattingListRenderer.jsx
+++ src/components/chat/chatListPanel/ChattingListRenderer.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import { useRouter } from 'next/navigation';
 
 export default function ChattingListRenderer({ listData }) {
-  console.log('listData.post >> ', listData);
   const router = useRouter();
 
   const handleClickChatRoom = () => {
