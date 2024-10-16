314d4cbc9f921167fc4d41d0aae7a27ac3df3afd
SMJ426 <jerry04260@gmail.com>
2024-10-16 23:50:56 +0900

    feat(PTFD):PTFD 페이지 개발

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ function WhatIsPTFD() {
   return (
     <div>
       <iframe
-        src="/PTFD 란 f83aa44142734578941f28be890cf8d6.html"
+        src="/제목 없음 f83aa44142734578941f28be890cf8d6.html"
         style={{ width: '100%', height: '100vh', border: 'none' }}
       />
     </div>
