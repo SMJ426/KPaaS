1aa87f4a5ca4ad653d825aae5007975867f17445
SMJ426 <jerry04260@gmail.com>
2024-10-20 17:36:06 +0900

    chore(delete): 사용하지 않는 import 제거

--- src/app/about/layout.jsx
+++ src/app/about/layout.jsx
@@ -1,7 +1,6 @@
 'use server';
 import MainNavigation from '@compoents/components/layout/main-navigation';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export default async function What_Is_PTFD({ children }) {
   const cookieStore = cookies();
